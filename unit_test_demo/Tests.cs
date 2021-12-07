2f3e0094f5ff7aaa541ca9a0f772076e2d23bb18
Brijesh <91509570+jeshmar@users.noreply.github.com>
2021-12-07 14:39:24 +1100

    create test.cs

---
+++ unit_test_demo/Tests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace unit_test_demo
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {   Assert.True(true);
+    }
+    }
+}
